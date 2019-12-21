dabb7c3a20a9858d399d56f609b20ed3cabc56e8
omarkhodier <khodieromar@gmail.com>
2019-12-21 18:55:01 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule ,ReactiveFormsModule } from "@angular/forms";
+import {CustomFormsModule} from 'ng2-validation';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { PriceComponent } from './price/price.component';
+
+const appRoutes: Routes = [
+ { path:'' , component: HomeComponent},
+ {path:'price' , component:PriceComponent} 
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    PriceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }),
+    FormsModule,
+    CustomFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
